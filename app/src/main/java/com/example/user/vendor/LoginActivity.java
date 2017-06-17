5426c78ad702fc10eddc7eaedcb8b4994cb05b6e
Denisa M <moise_denisa09@yahoo.com>
2017-06-17 18:32:49 +0300

    02

--- app/src/main/java/com/example/user/vendor/LoginActivity.java
+++ app/src/main/java/com/example/user/vendor/LoginActivity.java
@@ -1,9 +1,11 @@
 package com.example.user.vendor;
 
 import android.content.Intent;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -11,9 +13,19 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
     }
-    public void gotoProductsListActivity (View view){
-        Intent intent= new Intent (this, ProductsListActivity.class);
+
+    public void gotoProductsListActivity(View view){
+        EditText e1;
+        EditText e2;
+        e1 = (EditText) findViewById(R.id.edit_login_password);
+        e2 = (EditText) findViewById(R.id.edit_login_username);
+        if (e1.equals(e2)){
+            Intent intent= new Intent(this,ProductsListActivity.class);
         startActivity(intent);
+        }
     }
 }
+
--- app/src/main/java/com/example/user/vendor/ProductsListActivity.java
+++ app/src/main/java/com/example/user/vendor/ProductsListActivity.java
@@ -1,7 +1,9 @@
 package com.example.user.vendor;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ProductsListActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class ProductsListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_products_list);
+    }
+
+    public void gotoLoginActivity(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
